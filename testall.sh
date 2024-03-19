dd0b158c537d339d720c94eca0ff0a9c7b98d4ff
Marwan HOUSNI <marwan.housni@slurm-login-h23d5-u38-svn1.cloud.um6p.ma>
2024-03-19 17:23:03 +0000

    fix17

--- testall.sh
+++ testall.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#SBATCH --partition=gpu # partition name                                                                                                                                               
+#SBATCH --account=MANAPY-1WABCJWE938-DEFAULT-GPU                                                                                                                                           
+#SBATCH --export=NONE
+#SBATCH --gres=gpu:1                     # Necessary to activate the gpu card (The number of GPUs allowed by node is 1)
+##SBATCH --nodes=1
+#SBATCH -n 1                               # number of cores ( max 44 per node)
+#SBATCH --time=1-00:00:00                      # wall time to finish the job
+
+
+
+#SBATCH --job-name=X          # job name
+#SBATCH --output=tabsurvey-%j.log         # output file
+
+module load Anaconda3 CUDA/11.1.1
+
 N_TRIALS=2
 EPOCHS=3
 
@@ -20,7 +35,7 @@ MODELS=( ["LinearModel"]=$SKLEARN_ENV
          ["DecisionTree"]=$SKLEARN_ENV
          ["RandomForest"]=$SKLEARN_ENV
          ["XGBoost"]=$GBDT_ENV
-         ["CatBoost"]=$GBDT_ENV
+         ["CatBoost"]=$GBDT_ENV !!!!!!!!!!!!!!!!!!!!
          ["LightGBM"]=$GBDT_ENV
          ["MLP"]=$TORCH_ENV
          ["TabNet"]=$TORCH_ENV
@@ -31,8 +46,8 @@ MODELS=( ["LinearModel"]=$SKLEARN_ENV
          ["DeepGBM"]=$TORCH_ENV
          ["RLN"]=$KERAS_ENV
          ["DNFNet"]=$KERAS_ENV
-         ["STG"]=$TORCH_ENV
-         ["NAM"]=$TORCH_ENV
+         ["STG"]=$TORCH_ENV !!!!!!!!!!!!!!!!!!!!!!!!!!
+         ["NAM"]=$TORCH_ENV !!!!!!!!!!!!!!!!!
          ["DeepFM"]=$TORCH_ENV
          ["SAINT"]=$TORCH_ENV
          ["DANet"]=$TORCH_ENV
@@ -47,6 +62,7 @@ CONFIGS=( "config/adult.yml"
 # conda init bash
 eval "$(conda shell.bash hook)"
 
+
 for config in "${CONFIGS[@]}"; do
 
   for model in "${!MODELS[@]}"; do
@@ -55,7 +71,7 @@ for config in "${CONFIGS[@]}"; do
 
     conda activate "${MODELS[$model]}"
 
-    python train.py --config "$config" --model_name "$model" --n_trials $N_TRIALS --epochs $EPOCHS
+    python train.py --config "$config" --model_name "$model" --n_trials $N_TRIALS --epochs $EPOCHS 
 
     conda deactivate
 
